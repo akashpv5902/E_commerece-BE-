fef197cbed339e9a582fe9704a323b50ef3c75ba
akash5902 <akashpv592@gmail.com>
2023-02-08 12:33:46 +0530

    message

--- services/dataService.js
+++ services/dataService.js
@@ -80,13 +80,33 @@ deletewish=(id)=>{
     return db.wishlist.deleteOne({id}).then(
         (result)=>{
             if(result){
-                return{
-                    status:true,
-                    statusCode:200,
-                    products:result,
-                    message:"Product Removed"
+                // return{
+                //     status:true,
+                //     statusCode:200,
+                //     products:result,
+                //     message:"Product Removed"
 
-                }
+                // }
+                return db.wishlist.find().then(
+                    (result)=>{
+                        if(result){
+                            return{
+                                status:true,
+                                statusCode:200,
+                                wishlist:result,
+                                message:"product removed successfully"
+            
+                            }
+                        }
+                        else{
+                            return{
+                                status:false,
+                                statusCode:404,
+                                message:" wish-list is empty"
+                            }
+                        }
+                    }
+                )
             }
             else{
                 return{
